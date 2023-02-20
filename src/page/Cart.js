474388b16fbb4ff9fbcb8247af0dba1adb785bc2
tkrkr55 <tkrkr55@naver.com>
2023-02-20 15:13:09 +0900

    디자인

--- src/page/Cart.js
+++ src/page/Cart.js
@@ -22,15 +22,16 @@ export default function Cart() {
     <h1>장바구니</h1>
     </div>
     <div className='장바구니wrap'>
-    <Table  >
+    <Table>
      
   <thead >
     <tr>
       <th >상품명</th>
       <th >수량</th>
+      <th></th>
       <th >가격</th>
       <th></th>
-      
+      <th></th>
     </tr>
   </thead>
   
@@ -41,20 +42,36 @@ export default function Cart() {
       
       <td>
         <div className='imgendCart'><img className='cartimg' width={100} src={product[i].img}/><p
-      className='carttitle'> <strong>LoL Model</strong><br/><div className='무료배송'>무료배송 (내일출고)</div>{product[i].title}</p></div></td>
+      className='carttitle'> <strong>LoL Model</strong><br/><div className='무료배송'>무료배송 (내일출고)</div>{product[i].title}<p>{product[i].price}원</p></p></div></td>
       <td ><div className='cartcount'>{product[i].count}개</div></td>
       <td> <button className='plus' onClick={()=>{dispatch(pluscount(product[i].id))}}><FiPlusCircle/></button>
       
       <button  className='plus' onClick={()=>{dispatch(minus(product[i].id))}}> <FiMinusCircle/></button></td>
       <td><div className='cartPrice'>{product[i].count*product[i].price}원</div></td>
-      <td ><button onClick={()=>{dispatch(addfilter(product[i].id))}} ><FiX/></button></td>
+      <td ><button className='cartFix' onClick={()=>{dispatch(addfilter(product[i].id))}} ><FiX/></button></td>
       
 
     </tr>
     
    )}
+   {product.map((item,i)=>
+  <div className='cart_price'>
+   
+  <div className='cart_price_header'>
+  <div>총상품금액</div>
+  <div>총 배송비</div>
+  <h3>결제금액</h3>
 
-총 금액 {sum}
+  
+    <div>{sum}원</div>
+    <div> + 0원</div>
+    <h3>{sum}원</h3>
+  </div>
+  </div>
+
+   )}
+ 
+
    <div className='cartfoot'>
           <p>장바구니에 담긴 상품은 30일간 저장됩니다.<br/>
 L&M"은 자사/입점사 제품별, 주문유형별 배송비 기준에 따라 배송비가 별도 부과됩니다.<br/>
